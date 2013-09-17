58689787f857635fe7c66aaf20cf177240bbf869
Bassem Youssef <bassem@outlook.com>
2013-09-16 18:07:27 -0700

    add linear regression , sse and predict

--- src/mekena/multi-linear-reg.clj
+++ src/mekena/multi-linear-reg.clj
@@ -7,6 +7,8 @@
 ;; Stanford Machine Learning using Clojure and Incanter
 ;; machine learning multi-variate linear regression
 ;; config params
+;; ofcourse we can just do this, but what's the fun with that:
+;; (linear-model y (to-matrix (sel mtrx :cols [0 1])))
 (def alpha 0.01)
 (def iterations 1500)
 
@@ -17,102 +19,91 @@
 (def bedrooms (incore/sel mtrx :cols 1))
 (def price (incore/sel mtrx :cols 2))
 
-(incore/view (charts/scatter-plot
-              :col0
-              :col1
-              :data mtrx
-              :x-label "Population of City in 10,000s"
-              :y-label "Profit in $10,000s"))
+(defn calc-theta
+  [feat y theta]
+  (let [feat' (incore/trans feat)]
+    (-> feat
+        (incore/mmult theta)
+        (incore/minus y)
+        ((partial incore/mmult feat'))
+        (incore/div (count feat))
+        (incore/mult alpha)
+        ((partial incore/minus theta)))))
 
-(defn h-ftn
-  "The hypothesis function : X . theta
-  h-theta(x) = (theta0 * 1) + (theta1 * x1) + (theta2 * x2) ..
-  we add a column of 1 to x to produce a [m x (n+1)] matrix.
-  theta [(n+1) x 1], produces a matrix of [m x 1]"
-  [x theta]
-  (incore/mmult x theta))
+(defn gradient-descent
+  "Calculate gradient descent. Matrix contains multiple features with the y column
+  as the last column (the dependent variable).
+  An optional number of iterations can be passed as another parameter."
+  [iter y feats]
+  (drop (dec iter)
+        (take iter
+              (iterate (partial calc-theta feats y)
+                       (repeat (incore/ncol feats) 1)))))
 
-(defn prepare-x
-  "append a column of 1s as x0. Used as multiplier for theta0. Keep all cols except last"
+(defn prepare-features
+  "append a column of 1s as feature 0"
   [mtrx]
   (-> mtrx
       incore/nrow
       (repeat 1)
       vec
-      (incore/bind-columns (incore/to-matrix mtrx))
-      (incore/sel :except-cols (incore/ncol mtrx))))
+      (incore/bind-columns mtrx)))
 
-(defn calc-new-theta
+(defn normalize
+  "normalize a feature by subtracting the mean and scale by the std dev"
+  [mtrx]
+  (let [mean (stats/mean mtrx)
+        sd (stats/sd mtrx)]
+    (vec (map (comp (fn [n] (/ n sd)) (fn [n] (- n mean))) mtrx))))
+
+(defn linear-regression
+  "Calculate gradient descent. Matrix contains multiple features with the y column
+  as the last column (the dependent variable).
+  An optional number of iterations can be passed as another parameter."
+  ([mtrx] (linear-regression mtrx 1500))
+  ([mtrx iters] (let [y (incore/sel mtrx :cols (dec (incore/ncol mtrx)))
+                      feats (incore/sel mtrx :except-cols (dec (incore/ncol mtrx)))]
+                  (->> (incore/ncol feats)
+                       range
+                       (map (fn [c] (normalize (incore/sel feats :cols c))))
+                       matrix
+                       trans
+                       prepare-features
+                       (gradient-descent iters y)))))
+
+(defn predict-value
+  "fvec is a vector containing features to be predicted, mtrx is features and y"
+  [fvec mtrx]
+  (let [y (incore/sel mtrx :cols (dec (incore/ncol mtrx)))
+        feats (incore/sel mtrx :except-cols (dec (incore/ncol mtrx)))
+        features-stats (->> (incore/ncol feats)
+                            range
+                            (map (comp (juxt stats/mean stats/sd)
+                                       (fn [c] (incore/sel feats :cols c)))))
+        ;; append 1 for x0 (for intercept)
+        normalized-inputs (cons 1 (map (fn [fval [mean sd]] (/ (- fval mean) sd))
+                                       fvec features-stats))]
+    (as-> mtrx _
+      (linear-regression _)
+      (reduce
+        (fn [acc [coef inp]] (+ acc (* coef inp))) 0
+        (map vector
+             (map (fn [c] (first (incore/sel _ :cols c))) (range (incore/nrow (first _))))
+             normalized-inputs)))))
+
+(defn compute-mse
+  "Calculate the cost (mse : mean square error) function:
+  1/2m * (X*Theta - Y)^T.(X*Theta - Y)"
   [x y theta]
-  (let [x' (incore/trans x)]
-    (-> x
-        (incore/mmult theta)
-        (incore/minus y)
-        ((partial incore/mmult x'))
-        (incore/div (count x))
-        (incore/mult alpha)
-        ((partial incore/minus theta)))))
-
-;(defn calc-theta
-  ;[x y theta]
-  ;(map - theta ((juxt (partial calc-theta0 x y) (partial calc-theta1 x y)) theta)))
-
-(defn gradient-descent-history
-  "Get theta values for last 10 iterations, for debugging really"
-  [x y]
-  (drop (- iterations 10) (take iterations (iterate (partial calc-theta x y) [0 1]))))
-
-(defn gradient-descent
-  "Calculate gradient descent."
-  ([mtrx] (gradient-descent mtrx iterations))
-  ([mtrx iter]
-   (let [x (prepare-x mtrx)
-         y (incore/sel mtrx :cols (dec (incore/ncol mtrx)))]
-     (drop (dec iter)
-           (take iter
-                 (iterate (partial calc-new-theta x y)
-                          (repeat (incore/ncol mtrx) 1)))))))
-
-;(defn predict-value
-  ;[x xs ys]
-  ;(as-> xs _
-    ;(gradient-descent _ ys)
-    ;(flatten _)
-    ;(+ (first _) (* x (second _)))))
-
-;(defn compute-mse
-  ;"Calculate the cost (mse : mean square error) function: 1/2m * Sigma(from 1,m) (Theta^T*X - Y)^2"
-  ;[x y theta]
-  ;(let [t0 (first theta)
-        ;t1 (second theta)]
-    ;(as-> x _
-      ;(h-ftn t0 t1 _)
-      ;(map - _ y)
-      ;(map (fn [n] (* n n)) _)
-      ;(reduce + _)
-      ;(/ _ (* 2 (count x))))))
-
-;(defn calc-mse-hist
-  ;"Verify that mean error square to decreasing"
-  ;[x y]
-  ;(->> y
-       ;(gradient-descent-history x)
-       ;(map (partial compute-mse x y))))
-
-;;; now plot the regression
-;(defn plot-linear-reg
-  ;[x y]
-  ;(let [view (charts/scatter-plot
-               ;:col0
-               ;:col1
-               ;:data mtrx
-               ;:x-label "Population of City in 10,000s"
-               ;:y-label "Profit in $10,000s")
-        ;slope-incpt (first (gradient-descent x y))
-        ;intercept (first slope-incpt)
-        ;slope (second slope-incpt)]
-    ;(incore/view (charts/add-function
-                  ;view
-                  ;(fn [x] (+ intercept (* x slope)))
-                  ;(apply min x)
-                  ;(apply max x)))))
+  (let [normalized-x (->> (incore/ncol x)
+                          range
+                          (map (fn [c] (normalize (incore/sel x :cols c))))
+                          matrix
+                          trans
+                          prepare-features)
+        h-fn (incore/mmult normalized-x theta)]
+    (as-> h-fn _
+      (incore/minus _ y)
+      ((juxt incore/trans identity) _)
+      (incore/mmult (first _) (second _))
+      (incore/div _ (* 2 (incore/nrow x))))))
